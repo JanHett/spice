528491d663de4d4ce585014b1e5c856a2109babc
Jan Hettenkofer <jan@hettenkofer.net>
2019-10-29 00:44:29 +0100

    Switched order of build instructions in CI script

--- ci/travis.sh
+++ ci/travis.sh
@@ -4,5 +4,5 @@ mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../../spice_install
 cmake --build . --config Debug
+tests/spice-test
 cmake --build . --config Debug --target doc
-./tests/spice-test
