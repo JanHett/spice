ece007ddfdb709219240d5e579e0e20572884ccb
Jan Hettenkofer <jan@hettenkofer.net>
2020-03-22 21:25:21 +0100

    Fix GTEST version

--- ci/install-osx.sh
+++ ci/install-osx.sh
@@ -12,6 +12,7 @@ pip3 install jinja2 Pygments
 # clone, compile and make Google test
 git clone https://github.com/google/googletest.git ./external/gtest
 cd ./external/gtest
+git checkout tags/release-1.10.0
 mkdir build
 cd build
 cmake -Dgtest_build_samples=OFF -Dgtest_build_tests=OFF ../
